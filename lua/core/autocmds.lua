c2c32206f56f69c17fdc1e2a42f9fcf9a9212447
Alquimas <dinizmichel12@gmail.com>
2023-11-20 18:20:43 -0300

    .

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -3,9 +3,9 @@ local autocmd = vim.api.nvim_create_autocmd
 -- Destaca conteudo copiado
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
+    callback = function()
+        vim.highlight.on_yank{timeout=500}
+    end,
+    group = highlight_group,
+    pattern = "*",
 })
