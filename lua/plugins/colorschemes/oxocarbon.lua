d6831106255f35614022a05a6051a556a90ed8ea
Alquimas <dinizmichel12@gmail.com>
2024-02-09 17:59:39 -0300

    Fixed oxocarbon

--- lua/plugins/colorschemes/oxocarbon.lua
+++ lua/plugins/colorschemes/oxocarbon.lua
@@ -1,4 +1,32 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local hl = vim.api.nvim_set_hl
+
 return {
     "nyoom-engineering/oxocarbon.nvim",
     lazy = false,
+    config = function()
+        local colorscheme = augroup("ColorScheme", { clear = true })
+        autocmd("ColorScheme", {
+            group = colorscheme,
+            pattern = "*",
+            callback = function()
+                hl(0, "DiagnosticWarn", { fg = '#ffee00', bg = '#161616' })
+                hl(0, "DiagnosticError", { fg = '#ff0015', bg = '#161616' })
+                hl(0, "DiagnosticInfo", { fg = '#3094ff', bg = '#161616' })
+                hl(0, "DiagnosticHint", { fg = '#00ff11', bg = '#161616' })
+                hl(0, "DiagnosticUnderlineWarn",
+                    { fg = '#ffee00', bg = '#161616', undercurl = true })
+                hl(0, "DiagnosticUnderlineError",
+                    { fg = '#ff0015', bg = '#161616', undercurl = true })
+                hl(0, "DiagnosticUnderlineInfo",
+                    { fg = '#3094ff', bg = '#161616', undercurl = true })
+                hl(0, "DiagnosticUnderlineHint",
+                    { fg = '#00ff11', bg = '#161616', undercurl = true })
+                hl(0, "SignColumn", { fg = '#00685B', bg = '#161616' })
+                hl(0, "FoldColumn", { fg = '#ffffff', bg = '#161616' })
+                hl(0, "Folded", { fg = '#393935', bg = '#161616', italic = true })
+            end,
+        })
+    end
 }
